a36257ad14f8cfbc2739e9daafc568c96dcf7cb7
mirana <golovanshalom@gmail.com>
2023-03-10 15:19:47 +0400

    switch case changes

--- src/LR2.java
+++ src/LR2.java
@@ -4,7 +4,9 @@ public class LR2 {
         Vector a = new Vector(x0.get(0), x0.get(1));
         Vector b = new Vector(x1.get(0), x1.get(1));
         Vector dx;
+        int counter = 0;
         while (x1.sub(x0).dimension() >= eps) {
+            counter++;
             dx = b.sub(a).mul(Const.iPhi);
             x0 = b.sub(dx);
             x1 = a.add(dx);
@@ -13,7 +15,9 @@ public class LR2 {
             else
                 b = x1;
         }
+       System.out.println("Количество итераций : " + counter);
         dx = x1.add(x0).mul(0.5);
         return dx;
-    }
+   }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
@@ -7,7 +6,7 @@ public class Main {
         return ((X - 5) * (X - 2));
     }
     public static double testF2(Vector x) {
-        return (x.get(0) - 5) * x.get(0) + (x.get(1) - 3) * x.get(1); // min at point x = 2.5, y = 1.5
+        return (x.get(0) - 5) * x.get(0) + (x.get(1) - 3) * x.get(1);
     }
     public static final Function f = Main::testF;
     public static final Function2 f2 = Main::testF2;
@@ -17,7 +16,7 @@ public class Main {
         System.out.print("Enter lab work = ");
         Scanner sc = new Scanner(System.in);
         int c = sc.nextInt();
-        switch (c){
+        switch (c) {
             case 1:
                 System.out.print("Enter xL = ");
                 double xL = sc.nextDouble();
@@ -27,14 +26,17 @@ public class Main {
                 System.out.println("Половинное деление: " + LR1.dihtomia(f, xL, xR, Const.eps));
                 System.out.println("Золотое сечение: " + LR1.gRatio(f, xL, xR, Const.eps));
                 System.out.println("Фибоначчи: " + LR1.fibonacci(f, xL, xR, Const.eps));
+                break;
             case 2:
                 Vector x0 = new Vector(5.0, 3.0);
                 Vector x1 = new Vector(0.0, 0.0);
-                if(x0.size()!= x1.size())
+                if (x0.size() != x1.size())
                     System.out.println("Fake data");
-                else{
+                else
                     System.out.println("Золотое сечение: " + LR2.gRatio(f2, x0, x1, Const.eps).toString());
-                }
+                break;
+            default:
+                System.out.println("Fake data");
         }
     }
 
